2867df4807409b8a2cd5b8f75fab1686722bee4f
arseniyus <r.kozachenko2000@gmail.com>
2024-09-02 13:12:01 +0100

    "simpleton database added"

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource 
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 # We are wrapping our App in an API, initialises resful API
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"message":"Hello World"}
+#class HelloWorld(Resource):
+    #def get(self):
+        #return {"message":"Hello World"}
+
+@app.route("/", methods=["GET"])
+def test():
+    if request.method == "GET":
+        return jsonify({"response": "Hello There!"})
     
 # The response need to be in a valid JSON format
 # This creates a dictionary with a key "message" and a value "Hello World". 
     
-api.add_resource(HelloWorld, "/")
+# api.add_resource(HelloWorld, "/")
     # or /helloworld 
         
 if __name__ == "__main__":
